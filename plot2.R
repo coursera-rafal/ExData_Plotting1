7a10da3cb6316364c5ec124828f58f4f10a3dadc
coursera-rafal <33069741+coursera-rafal@users.noreply.github.com>
2018-12-17 20:32:22 +0000

    Add files via upload
    Assignment files

---
+++ plot2.R
@@ -0,0 +1,30 @@
+DownloadURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+DownloadFile <- "./household_power_consumption.zip"
+TxtFile <- "./Data/household_power_consumption.txt"
+
+
+# Download source ---------------------------------------------------------
+if (!file.exists(TxtFile)) {
+  download.file(DownloadURL, DownloadFile)
+  unzip(DownloadFile, overwrite = T, exdir = "./Data")
+}
+
+
+# Read data ---------------------------------------------------------------
+ReadData <- read.csv(TxtFile, sep=';', na.strings="?",skip = 66636)
+colnames(ReadData) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage",
+                        
+                        "Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+
+# Subset dates ------------------------------------------------------------
+Data <- subset(ReadData, Date %in% c("1/2/2007","2/2/2007"))
+Data$Date <- as.Date(Data$Date, format="%d/%m/%Y")
+Data$DateTime <-as.POSIXct(paste(Data$Date, Data$Time))
+
+# Create png chart --------------------------------------------------------
+png("plot2.png", width=480, height=480)
+with(Data, {
+  plot(Global_active_power~DateTime, type="l",
+       ylab="Global Active Power (kilowatts)", xlab="")
+})
+dev.off()
